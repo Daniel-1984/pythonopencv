d7ebb419bd0dbb3fecd97ca3ccd2ba65d92a98e4
Daniel Luiz Alves <56437608+Daniel-1984@users.noreply.github.com>
2022-10-01 17:17:31 -0400

    Add files via upload

--- contadorvagas.py
+++ contadorvagas.py
@@ -16,19 +16,22 @@ while True:
     kernel = np.ones((3,3),np.int8)
     imgDil = cv2.dilate(imgMedian,kernel)
 
+
+    vagasAbertas = 0
     for x,y,w,h in vagas:
-        vaga = imgDil[y:y+h,x+w]
+        vaga = imgDil[y:y+h,x:x+w]
         count = cv2.countNonZero(vaga)
         cv2.putText(img,str(count),(x,y+h-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
         
-        if count <6:
-        
-            cv2.rectangle(img,(x, y), (x + w, y + h),(0, 255, 0),2)
+        if count <900:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            vagasAbertas +=1
+
 
         else:
-             cv2.rectangle(img,(x, y), (x + w, y + h),(0, 0, 255),2)
-        
-
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.rectangle(img,(90,0), (415, 60), (0, 255, 0), -1)
+                cv2.putText(img, f'livre: {vagasAbertas}/69' ,(95,45), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 5)
 
 
     cv2.imshow('video',img)
